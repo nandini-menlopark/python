d3b78cde657ac6f94d9b9fc2b234662408ed24d1
nandini-menlopark <68158271+nandini-menlopark@users.noreply.github.com>
2020-07-18 19:37:50 -0400

    Create storyGenerator.py

---
+++ storyGenerator.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 18 19:01:27 2020
+
+@author: cruise
+"""
+
+def getPronoun(gender):
+    if(gender in ['boy', 'young man', 'man']):
+        return('He')
+    
+    return('She')
+
+
+
+def writeStory(name, age, gender, grade, city, hobby, book, friendName, brotherOrSister):
+    story = f"""{name} is a very nice {gender}. 
+    {getPronoun(gender)} lives in {city}. 
+    {name}'s favorite activity is {hobby}. 
+    {name}, who is {age} years old, is going to attend {grade} grade after the summer gets over. 
+    {name}'s favorite book is {book}. 
+    {name}'s best friend is {friendName}. 
+    {name} has a sibling, and it is a {brotherOrSister}."""
+    
+    return(story)
+
+
+
+if __name__ == '__main__':
+    story = writeStory('Aryan', '17', 'young man', 
+                       '10th', 'London', 'Karate', 
+                       'Detective Stories', 'Bhootnath', 'sister')
+    #story = writeStory('Mayur', '5', 'boy', 'KG', 'Edison', 'blocks', 'KG Learning', 'Daksh', 'Sister')
+    print(story)
