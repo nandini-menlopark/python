6c6861e9278cb1f0c78c23d6e657c3067f49a974
vishv-jeet <vjeet@burgiss.com>
2020-07-27 18:21:30 -0400

    Expression Solver

---
+++ ExpressionSolver.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 17:20:14 2020
+
+@author: cruise
+"""
+
+
+class ExpressionSolver(object):
+    def __init__(self):
+        self.a  = 0
+        self.b = 0
+                
+    def getA(self):
+        return(self.a)
+    
+    def getB(self):
+        return(self.b)
+    
+    def getOperation(self):
+        return(self.op)
+    
+    def getResult(self):
+        if(self.op == '+'):
+            return(self.a + self.b)
+        
+        if(self.op == '-'):
+            return(self.a - self.b)
+        
+        if(self.op == '*'):
+            return(self.a * self.b)
+        
+        if(self.op == '/'):
+            return(self.a / self.b)
+        
+        if(self.op == '%'):
+            return(self.a % self.b)
+        
+        if(self.op == '>'):
+            return(self.a > self.b)
+
+        if(self.op == '<'):
+            return(self.a < self.b)
+        
+        if(self.op == '^'):
+            return(self.a ** self.b)
+
+        if(self.op == 'e'):
+            return(self.a == self.b)
+        
+        if(self.op == 'n'):
+            return(self.a != self.b)
+        
+        if(self.op == 'G'):
+            return(self.a >= self.b)
+        
+        if(self.op == 'L'):
+            return(self.a <= self.b)
+        
+    def solveExpression(self, expr):
+        self.expr = expr.replace(' ', '')
+        i = 0
+        while(True):
+            if self.expr[i].isnumeric() == False and self.expr[i] != '.':
+                self.op = self.expr[i]
+                break
+            i = i + 1
+        mysplit = self.expr.split(self.op)
+        self.a = float(mysplit[0])
+        self.b = float(mysplit[1])
+        return(self.getResult())
+                
+
+if __name__ == '__main__':
+    
+    myExprSolver = ExpressionSolver()
+    
+    expr = '500 L 200'
+    
+    print(f'Answer is {myExprSolver.solveExpression(expr)}')
+    
